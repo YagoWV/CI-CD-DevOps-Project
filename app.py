e922139fed394e6a268ef695b1b1c58071d5f712
YagoWV <yagowerneckvane@gmail.com>
2024-09-16 23:07:04 -0300

    Adicionando Dockerfile, requirements.txt, e modificações no README e app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-print("Hello, CI/CD!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
 def hello():
-    print("Hello from a function!")
-hello()
+    return "Hello, Docker!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
